6639d9fa54a0b9427e0b2ea9a4f953d00c736f9a
2222nit <nitishkamboj8070@gmail.com>
2026-02-24 12:36:05 +0530

    sd

--- studio/src/Components/AboutUs.jsx
+++ studio/src/Components/AboutUs.jsx
@@ -5,18 +5,25 @@ import ScrollIndicator from "./ScrollIndicator";
 export default function AboutUs() {
   return (
     <>
-      <div className="bg-[#060010] h-1000 top-0">
-        <section className=" relative flex justify-center">
-          <img src={Img1} alt="" className=" h-100 md:h-150 md:w-182 lg:h-150 lg:w-250 xl:h-220 xl:w-420 2xl:h-168 2xl:w-460 md:my-5 lg:m-5  " />
-          <div className=" absolute top-65 md:top-120 md:left-8 lg:top-105 lg:left-12 xl:top-165 2xl:top-120 2xl:left-12 text-balance md:w-150 lg:w-230 xl:w-300 2xl:w-320 ">
+      <div className="bg-[#060010] h-1000 top-0 flex justify-center">
+        <section className=" relative flex flex-col px-8">
+          <img src={Img1} alt="" className=" h-100 md:h-150 md:w-182 lg:h-150 lg:w-245 xl:h-215 xl:w-350 2xl:h-220 2xl:w-screen md:my-5 lg:m-5 2xl:my-8 2xl:mx-0 " />
+          <div className=" absolute top-65 md:top-120 md:left-8 lg:top-105 lg:left-12 xl:top-165 2xl:top-140 2xl:left-12 text-balance md:w-150 lg:w-230 xl:w-300 2xl:w-7xl ">
             <h3 className="text-red-500 font-cormorant uppercase text-xs md:text-lg text-center md:text-start">Bay Area Indian wedding photographer</h3>
             <h1 className="text-white text-2xl md:text-4xl lg:text-5xl xl:text-6xl font-cormorant uppercase text-balance text-center md:text-start">
               Capturing natural moments of joy & celebration
             </h1>
           </div>
-          <div className=" hidden lg:flex absolute top-138 left-125 xl:top-205 xl:left-180 2xl:top-155 2xl:left-190 ">
+         <div>
+           <div className="flex justify-center">
+            {/* <div className=" hidden lg:flex absolute top-138 left-125 xl:top-205 xl:left-180 2xl:top-165 2xl:left-185 text-center">
+            <ScrollIndicator />
+          </div> */}
+            <div className="absolute top-170">
             <ScrollIndicator />
           </div>
+           </div>
+         </div>
         </section>
       </div>
     </>
--- studio/src/Components/Navbar.jsx
+++ studio/src/Components/Navbar.jsx
@@ -24,46 +24,48 @@ export default function Navbar() {
   ];
   return (
     <>
+        <div className="flex justify-center">
       <header className="absolute top-5 2xl:top-10 z-50 bg-transparent ">
-        <nav className=" h-12 md:h-15 p-1 md:mx-2 lg:mx-8 xl:mx-12 2xl:mx-40 rounded-4xl bg-black/20 shadow-2xl shadow-black text-[18px] font-serif w-screen md:w-188 lg:w-240 xl:w-306 2xl:w-300 ">
-          <ul className=" hidden md:flex justify-around h-13 items-center md:text-xl md:mx-3">
-            {navBar.map((v, i) => (
-              <li key={i}>
-                <Link
-                  to ={v.href}
-                  className=" h-1 pb-3 px-2 text-white hover:text-white font-cormorant  md:text-lg lg:text-xl transition-discrete ease-in-out duration-80 hover:border-b-4 border-red-500"
-                >
-                  {v.name}
-                  {v.image && (
-                    <img
-                      src={v.image}
-                      alt="logo"
-                      className="h-30 w-auto p-0 hover:border-none border-black.  "
-                    />
-                  )}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <ul className=" md:hidden h-10 flex justify-between items-center mx-3">
-            <li>
-              <img src={Img} alt="Logo" className="h-15 w-auto" />
-            </li>
-            <li className="text-white text-[26px] w-7" onClick={togglemenu}>
-              {open ? <RxCross2 /> : <CiMenuBurger />}
-            </li>
-          </ul>
-          {open && (
-            <ul className="md:hidden px-3 pt-2 h-fit w-auto text-white flex flex-col gap-1.5 text-end justify-end duration-300 transition-all">
-              {mobileNav.map((v, i) => (
-                <li key={i} className="">
-                  <Link to ={v.href}>{v.name}</Link>
+          <nav className=" h-12 md:h-15 p-1 md:mx-2 lg:mx-8 xl:mx-12 2xl:mx-40 rounded-4xl bg-black/20 shadow-2xl shadow-black text-[18px] font-serif w-screen md:w-188 lg:w-240 xl:w-300 2xl:w-300 ">
+            <ul className=" hidden md:flex justify-around h-13 items-center md:text-xl md:mx-3">
+              {navBar.map((v, i) => (
+                <li key={i}>
+                  <Link
+                    to={v.href}
+                    className=" h-1 pb-3 px-2 text-white hover:text-white font-cormorant  md:text-lg lg:text-xl transition-discrete ease-in-out duration-80 hover:border-b-4 border-red-500"
+                  >
+                    {v.name}
+                    {v.image && (
+                      <img
+                        src={v.image}
+                        alt="logo"
+                        className="h-30 w-auto p-0 hover:border-none border-black.  "
+                      />
+                    )}
+                  </Link>
                 </li>
               ))}
             </ul>
-          )}
-        </nav>
+            <ul className=" md:hidden h-10 flex justify-between items-center mx-3">
+              <li>
+                <img src={Img} alt="Logo" className="h-15 w-auto" />
+              </li>
+              <li className="text-white text-[26px] w-7" onClick={togglemenu}>
+                {open ? <RxCross2 /> : <CiMenuBurger />}
+              </li>
+            </ul>
+            {open && (
+              <ul className="md:hidden px-3 pt-2 h-fit w-auto text-white flex flex-col gap-1.5 text-end justify-end duration-300 transition-all">
+                {mobileNav.map((v, i) => (
+                  <li key={i} className="">
+                    <Link to={v.href}>{v.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </nav>
       </header>
+        </div>
     </>
   );
 }
