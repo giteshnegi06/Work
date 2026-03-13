43c7969cfb4df6476b4f35ffd5ffd652e70f2c6c
2222nit <nitishkamboj8070@gmail.com>
2026-03-13 10:07:59 +0530

    as

--- studio/src/Components/Contact.jsx
+++ studio/src/Components/Contact.jsx
@@ -10,9 +10,9 @@ export default function Contact() {
             <div className="bg-black h-fit top-0 flex justify-center items-center">
                 <section className=" relative flex flex-col justify-center items-center md:mx-5 2xl:px-8 h-fit">
                     <img src="https://res.cloudinary.com/dbtlo70e5/image/upload/v1772900613/0P0A9756.JPG_im87u7.jpg" alt=""
-                        className=" h-90  md:h-150 lg:h-180 2xl:h-215 w-screen md:m-5 lg:m-6 2xl:m-8 object-cover" />
+                        className=" h-90  md:h-screen w-screen md:m-5 lg:m-6 2xl:m-8 object-cover" />
                     <div className="absolute inset-0 bg-black/60 2xl:m-8 "></div>
-                    <div className=" absolute flex flex-col bottom-5 md:bottom-10 lg:bottom-10 text-balance text-center items-center lg:w-150 2xl:w-180 text-white">
+                    <div className=" absolute flex flex-col bottom-5 md:bottom-10 lg:bottom-12 text-balance text-center items-center lg:w-150 2xl:w-180 text-white">
                         <h3 className="text-red-600 font-cormorant lowercase md:text-2xl text-center tracking-wide">
                             LET’S DISCUSS HOW WE CAN
                         </h3>
@@ -21,7 +21,7 @@ export default function Contact() {
                         </h1>
                         <p className='font-cormorant text-xs md:text-lg'>For wedding & events, please fill the contact form below.</p>
                         <p className='font-cormorant text-xs md:text-lg'>For all other inquiries, please email us directly at mahakstudio2000@gmail.com</p>
-                        <ScrollIndicator />
+                        <ScrollIndicator className='hidden lg:flex'/>
                     </div>
                 </section>
 
@@ -29,7 +29,7 @@ export default function Contact() {
             <div className="bg-[#060010] flex justify-center items-center text-white p-20">
                 <ContactForm />
             </div>
-            <div className=''>
+            <div className='overflow-hidden'>
                 <Main />
             </div>
         </>
--- studio/src/Components/Portfolio.jsx
+++ studio/src/Components/Portfolio.jsx
@@ -1,31 +1,51 @@
 // Portfolio.js
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 import Whatsaap from "./Whatsaap";
-import PortfolioGallery from './PortfolioGallery';
-import PortfolioVideos from './PortfolioVideos';
+import PortfolioGallery from "./PortfolioGallery";
+import PortfolioVideos from "./PortfolioVideos";
 
 export default function Portfolio() {
-
   const wedding = [
-    { slug: "taranveer-bhupinder", image: "https://res.cloudinary.com/dbtlo70e5/image/upload/v1772377745/042A6463_n4pbpv.jpg", alt: "Taranveer Singh & Bhupinder Kaur", title: "Taranveer Singh & Bhupinder Kaur" },
-    { slug: "amandeep-pooja", image: "https://res.cloudinary.com/dbtlo70e5/image/upload/v1772900625/0P0A9781.JPG_wma6no.jpg", alt: "Amandeep & Pooja", title: "Amandeep & Pooja" },
+    {
+      slug: "taranveer-bhupinder",
+      image:
+        "https://res.cloudinary.com/dbtlo70e5/image/upload/v1772377745/042A6463_n4pbpv.jpg",
+      alt: "Taranveer Singh & Bhupinder Kaur",
+      title: "Taranveer Singh & Bhupinder Kaur",
+    },
+    {
+      slug: "amandeep-pooja",
+      image:
+        "https://res.cloudinary.com/dbtlo70e5/image/upload/v1772900625/0P0A9781.JPG_wma6no.jpg",
+      alt: "Amandeep & Pooja",
+      title: "Amandeep & Pooja",
+    },
   ];
   const preWedding = [
-    { slug: "amit-tanvi", image: "https://res.cloudinary.com/dbtlo70e5/image/upload/v1772378530/5_r0yfxo.jpg", alt: "Image 1", title: "Amit & Tanvi" },
+    {
+      slug: "amit-tanvi",
+      image:
+        "https://res.cloudinary.com/dbtlo70e5/image/upload/v1772378530/5_r0yfxo.jpg",
+      alt: "Image 1",
+      title: "Amit & Tanvi",
+    },
   ];
 
   return (
     <>
       <Whatsaap />
       <div>
-        <img src="https://res.cloudinary.com/dbtlo70e5/image/upload/v1772474708/753_bzn068.jpg"
-          alt="Pre-Wedding Shoot by Mahak Studio" className=' h-150  md:h-150 md:w-195 lg:h-160 lg:w-260 xl:h-189 xl:w-360 2xl:h-230 2xl:w-screen object-cover' />
+        <img
+          src="https://res.cloudinary.com/dbtlo70e5/image/upload/v1772474708/753_bzn068.jpg"
+          alt="Pre-Wedding Shoot by Mahak Studio"
+          className=" h-150  md:h-150 md:w-195 lg:h-160 lg:w-260 xl:h-189 xl:w-360 2xl:h-230 2xl:w-screen object-cover"
+        />
       </div>
       <PortfolioGallery />
 
       {/* Wedding Section */}
-      <div className="bg-black flex justify-center py-5 md:py-20">
+      <div className="bg-black flex justify-center py-5 md:py-20 ">
         <div className="flex flex-col items-center">
           <h1 className="text-white text-4xl md:text-6xl xl:text-7xl font-bold mb-10 font-dancing ">
             Wedding's
@@ -34,7 +54,11 @@ export default function Portfolio() {
             {wedding.map((v, i) => (
               <Link key={i} to={`/wedding/${v.slug}`}>
                 <div className="group relative w-70 h-105 rounded-2xl overflow-hidden cursor-pointer">
-                  <img src={v.image} alt={v.alt} className=" object-cover transition-transform duration-500 group-hover:scale-110" />
+                  <img
+                    src={v.image}
+                    alt={v.alt}
+                    className=" object-cover transition-transform duration-500 group-hover:scale-110"
+                  />
                   <div className="absolute inset-0 group-hover:bg-black/50 transition duration-500"></div>
                   <h2 className="absolute bottom-10 left-0 right-0 text-white text-center text-3xl md:text-4xl font-dancing z-10">
                     {v.title}
@@ -54,9 +78,15 @@ export default function Portfolio() {
           </h1>
           <div className="flex flex-wrap items-center justify-center gap-10 md:px-4 mt-10">
             {preWedding.map((v, i) => (
-              <Link key={i} to={`/wedding/${v.slug}`}> {/* Fixed: used v.slug instead of wedding.slug */}
+              <Link key={i} to={`/wedding/${v.slug}`}>
+                {" "}
+                {/* Fixed: used v.slug instead of wedding.slug */}
                 <div className="group relative w-70 h-105 rounded-2xl overflow-hidden cursor-pointer">
-                  <img src={v.image} alt={v.alt} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" />
+                  <img
+                    src={v.image}
+                    alt={v.alt}
+                    className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                  />
                   <div className="absolute inset-0 group-hover:bg-black/50 transition duration-500"></div>
                   <h2 className="absolute bottom-10 left-0 right-0 text-white text-center text-3xl md:text-4xl font-dancing z-10">
                     {v.title}
@@ -70,5 +100,5 @@ export default function Portfolio() {
 
       <PortfolioVideos />
     </>
-  )
+  );
 }
